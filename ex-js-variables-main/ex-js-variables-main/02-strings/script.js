023f8b790637d812eba125aad2b7d4264e79dcdc
angelostori <angelostori13@gmail.com>
2025-10-03 15:49:39 +0200

    declare three different variables

--- ex-js-variables-main/ex-js-variables-main/02-strings/script.js
+++ ex-js-variables-main/ex-js-variables-main/02-strings/script.js
@@ -10,6 +10,11 @@ console.log(nome)
 // un numero, una stringa e un valore booleano.
 // Stampa tutti i valori nella console.
 
+const number = 30
+const text = "Hello World"
+const bool = true
+
+
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
 // e le variabili sopra in modo da stampare correttamente 
 // in console la stringa con il nome completo
